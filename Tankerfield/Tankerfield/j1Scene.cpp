714f4d8275fd588ca2e017825aaeb4d79c0db6a2
Sersius <segoro97@msn.com>
2019-03-19 19:56:36 +0100

    GetRotatedSprite bulletproof

--- Tankerfield/Tankerfield/j1Scene.cpp
+++ Tankerfield/Tankerfield/j1Scene.cpp
@@ -76,7 +76,7 @@ bool j1Scene::Update(float dt)
 		App->render->camera.x -= floor(200.0f * dt);
 
 	// Draw ------------------
-	App->render->Blit(texture, 20, 20, GetRotatedSprite(tesla_trooper_rects,8,90));
+	App->render->Blit(texture, 20, 20, GetRotatedSprite(tesla_trooper_rects,8,-730));
 
 	return true;
 }
@@ -102,8 +102,19 @@ bool j1Scene::CleanUp()
 	return true;
 }
 
+//The angle should be in degrees and rect_num is the number of rectangles that are inside rect
 SDL_Rect* j1Scene::GetRotatedSprite(SDL_Rect* rect, int rect_num, float angle)
 {
+	if (angle > 360) {
+		angle = fmod(angle, 360);
+	}
+	else if (angle < -360) {
+		angle = fmod(angle, -360);
+	}
+
+	if (angle < 0)
+		angle += 360;
+
 	float angle_part = 360 / rect_num;
 
 	float num_pos = angle / angle_part;
@@ -114,6 +125,9 @@ SDL_Rect* j1Scene::GetRotatedSprite(SDL_Rect* rect, int rect_num, float angle)
 		num_pos = num_pos - remainder + 1;
 	else
 		num_pos = num_pos - remainder;
+
+	if (num_pos == rect_num)
+		num_pos = 0;
 
 	return &rect[(int)num_pos];
 }
