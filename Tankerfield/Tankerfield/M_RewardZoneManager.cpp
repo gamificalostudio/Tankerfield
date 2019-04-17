924fa2e1d772ed69edce0f2bb3226871f78f7b6b
Gerard Marcos <gerardmarcosfreixas@gmail.com>
2019-04-17 23:21:27 +0200

    Code ready

--- Tankerfield/Tankerfield/M_RewardZoneManager.cpp
+++ Tankerfield/Tankerfield/M_RewardZoneManager.cpp
@@ -1,2 +1,77 @@
 #include "M_RewardZoneManager.h"
 
+/* Reward Zone */
+
+RewardZone::RewardZone()
+{
+
+}
+
+RewardZone::~RewardZone()
+{
+
+}
+
+
+/* Module RewardZoneManager */
+
+M_RewardZoneManager::M_RewardZoneManager()
+{
+
+}
+
+M_RewardZoneManager::~M_RewardZoneManager()
+{
+
+}
+
+bool M_RewardZoneManager::Awake(pugi::xml_node& config)
+{
+	return true;
+}
+
+bool M_RewardZoneManager::Start()
+{
+	return true;
+
+}
+
+bool M_RewardZoneManager::PreUpdate()
+{
+	return true;
+
+}
+
+bool M_RewardZoneManager::Update(float dt)
+{
+	return true;
+
+}
+
+bool M_RewardZoneManager::PostUpdate(float dt)
+{
+	return true;
+
+}
+
+bool M_RewardZoneManager::CleanUp()
+{
+	return true;
+
+}
+
+RewardZone* M_RewardZoneManager::CreateRewardZone(iPoint map_center_pos, uint size)
+{
+	RewardZone* test = nullptr;
+	return test;
+}
+
+void M_RewardZoneManager::DeleteRewardZones()
+{
+
+}
+
+std::list<RewardZone*> M_RewardZoneManager::GetRewardZones() const
+{
+	return this->reward_zones;
+}
