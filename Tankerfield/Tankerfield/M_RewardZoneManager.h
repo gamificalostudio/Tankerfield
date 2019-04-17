d7b9fc1f60fe126a1f985d344b5f33cb7787f035
Gerard Marcos <gerardmarcosfreixas@gmail.com>
2019-04-17 23:00:47 +0200

    Basic reward zone structure

---
+++ Tankerfield/Tankerfield/M_RewardZoneManager.h
@@ -0,0 +1,47 @@
+#ifndef __M_RewardZoneManager_H__
+#define __M_RewardZoneManager_H__
+
+#include <list>
+
+#include "Module.h"
+#include "Point.h"
+
+class RewardZone
+{
+public:
+	RewardZone();
+	RewardZone();
+
+private:
+
+};
+
+class M_RewardZoneManager : public Module
+{
+public:
+	M_RewardZoneManager();
+	~M_RewardZoneManager();
+
+	bool Awake(pugi::xml_node& config) override;
+
+	bool Start() override;
+
+	bool PreUpdate() override;
+
+	bool Update(float dt) override;
+
+	bool PostUpdate(float dt) override;
+
+	bool CleanUp() override;
+
+	RewardZone* CreateRewardZone(iPoint map_center_pos, uint size);
+
+	void DeleteRewardZones();
+
+	std::list<RewardZone*> GetRewardZones() const;
+
+private:
+	std::list<RewardZone*> reward_zones;
+};
+
+#endif /* __M_RewardZoneManager_H__ */
