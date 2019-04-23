28f1339b31d96ed618afebb7f448f9c92b208695
Yessica Tamara Servín Domínguez <yessicasd@TR12.CAT>
2019-04-23 19:57:42 +0200

    changed tesla teleport

--- Tankerfield/Tankerfield/Obj_TeslaTrooper.cpp
+++ Tankerfield/Tankerfield/Obj_TeslaTrooper.cpp
@@ -226,7 +226,7 @@ void Obj_TeslaTrooper::Movement(float &dt)
 		for (std::vector<SpawnPoint*>::iterator spawn_point = app->map->data.spawners_position_enemy.begin(); spawn_point != app->map->data.spawners_position_enemy.end(); ++spawn_point)
 		{
 			float distance_to_this_spawnpoint = this->pos_map.DistanceManhattan((*spawn_point)->pos);
-			if (distance_to_this_spawnpoint < distance_to_tank && target->pos_map.DistanceManhattan((*spawn_point)->pos))
+			if (target->pos_map.DistanceManhattan((*spawn_point)->pos))
 			{
 				if (nearest_spawners_points == nullptr || distance_to_this_spawnpoint < last_distance_to_spawnpoint)
 				{
