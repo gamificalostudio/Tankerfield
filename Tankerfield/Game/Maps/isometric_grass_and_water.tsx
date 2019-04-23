60ca3f8d335f1b180ca24efe10965428139363ee
AitorVelez <aitorvelez@hotmail.com>
2019-04-23 17:34:37 +0200

    RemovedUnusefulFiles and compressedaudios

--- Tankerfield/Game/Maps/isometric_grass_and_water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="isometric_grass_and_water" tilewidth="60" tileheight="30" tilecount="48" columns="4">
- <image source="isometric_grass_and_water.png" width="256" height="384"/>
-</tileset>
