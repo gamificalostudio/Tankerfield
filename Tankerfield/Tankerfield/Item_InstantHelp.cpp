1cef43669d382f96a4752a01e54e3e798cb80cd0
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-05-12 16:41:41 +0200

    Item Instant Help and Object Portal Created (crashes)

---
+++ Tankerfield/Tankerfield/Item_InstantHelp.cpp
@@ -0,0 +1,29 @@
+#include "Item_InstantHelp.h"
+#include "Object.h"
+#include "Obj_Tank.h"
+#include "Obj_Item.h"
+#include "App.h"
+#include "M_ObjManager.h"
+
+Item_InstantHelp::Item_InstantHelp(fPoint pos):Obj_Item(pos)
+{
+}
+
+bool Item_InstantHelp::Update(float dt)
+{
+	if(caster!=nullptr)
+	{
+		pos_map = caster->pos_map;
+	}
+
+	return true;
+}
+
+bool Item_InstantHelp::Use()
+{
+	fPoint casterdir = caster->GetShotDir();
+	(Obj_Portal*)app->objectmanager->CreateObject(ObjectType::PORTAL, pos_map + casterdir *3);
+
+	return true;
+}
+
--- Tankerfield/Tankerfield/Obj_Tank.cpp
+++ Tankerfield/Tankerfield/Obj_Tank.cpp
@@ -232,7 +232,7 @@ bool Obj_Tank::Start()
 	tutorial_pick_up->AddTextHelper("TAKE", { 0.f, 70.f });
 	tutorial_pick_up->SetStateToBranch(ELEMENT_STATE::HIDDEN);
 
-	SetItem(ItemType::HEALTH_BAG);
+	SetItem(ItemType::INSTANT_HELP);
 	return true;
 }
 
