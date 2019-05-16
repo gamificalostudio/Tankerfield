0f2aef9d617317fd8f60d8a6ac55220c66125f97
VictorSegura99 <36154682+VictorSegura99@users.noreply.github.com>
2019-05-16 14:26:09 +0200

    Brute now has the texture change correctly

--- Tankerfield/Tankerfield/Obj_Brute.cpp
+++ Tankerfield/Tankerfield/Obj_Brute.cpp
@@ -89,7 +89,14 @@ void Obj_Brute::ChangeTexture()
 {
 	if (spawn.Finished() && damaged_sprite_timer.Read() > damaged_sprite_time)
 	{
-		curr_tex = tex;
+		if (oiled == false)
+		{
+			curr_tex = tex;
+		}
+		else
+		{
+			curr_tex = oiled_tex;
+		}
 	}
 }
 
