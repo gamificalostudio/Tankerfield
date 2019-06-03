ea033bafc369e524f92600cbc77435a52137bd42
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-06-03 20:32:40 +0200

    added documents

---
+++ Tankerfield/Tankerfield/Controllers_Settings.h
@@ -0,0 +1,23 @@
+#ifndef _CONTROLLERS_SETTINGS_H_
+#define _CONTROLLERS_SETTINGS_H_
+#include "M_UI.h"
+
+class Controllers_Settings :public UI_Listener
+{
+public:
+	Controllers_Settings();
+	~Controllers_Settings();
+
+private:
+
+};
+
+Controllers_Settings::Controllers_Settings()
+{
+}
+
+Controllers_Settings::~Controllers_Settings()
+{
+}
+#endif // _CONTROLLERS_SETTINGS_H_
+
