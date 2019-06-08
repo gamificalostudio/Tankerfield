1a1bb30cddab030b74a0c496056a340d78e0b4c3
JaumeMontagut <36292291+JaumeMontagut@users.noreply.github.com>
2019-06-08 21:25:33 +0200

    adding particle pool

---
+++ Tankerfield/Tankerfield/ParticlePool.h
@@ -0,0 +1,32 @@
+#ifndef __PARTICLE_POOL_H__
+#define __PARTICLE_POOL_H__
+
+#include "Particle.h"
+#include "Emitter.h"
+#include "p2Point.h"
+#include <vector>
+
+class Emitter;
+
+class ParticlePool
+{
+private:
+	
+	int pool_size = 0;
+	Particle * first_available	= nullptr;
+	Particle * particle_array	= nullptr;
+
+
+public:
+
+	ParticlePool(Emitter* emitter);
+	virtual ~ParticlePool();
+
+	//Generates a new particle each time it's called
+	void Generate(fPoint pos, float startSpeed, float endSpeed, float angle, float rotSpeed, float startSize, float endSize, uint life, SDL_Rect textureRect, SDL_Color startColor, SDL_Color endColor, SDL_BlendMode blendMode);
+	
+	// Update (move and draw) particles in the pool. If there are no particles alive returns false
+	bool Update(float dt);
+};
+
+#endif
