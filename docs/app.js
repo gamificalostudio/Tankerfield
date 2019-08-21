214c436ebf7e73bd82f49282cbf9c25288452583
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-08-21 16:02:41 +0200

    added navigation bar

---
+++ docs/app.js
@@ -0,0 +1,32 @@
+const navSlide = () =>
+{
+    const burger = document.querySelector('.burger');
+    const nav= document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //Toggle Nav
+    burger.addEventListener('click', ()=>{
+        
+        //Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        //Animation Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation)
+            {
+                link.style.animation = '';
+            }
+            else
+            {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
+            }
+
+            console.log(index/7);
+        });
+        
+        //Burger Animation
+        burger.classList.toggle('toggle');
+
+    });
+
+}
+navSlide();
