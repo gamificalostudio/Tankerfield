2e5275d38b2c153852f5b4c868a618a710d7047f
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2019-08-25 01:36:43 +0200

    adding logo image

--- docs/Website/app.js
+++ docs/Website/app.js
@@ -1,32 +1,26 @@
-const navSlide = () =>
-{
-    const burger = document.querySelector('.burger');
-    const nav= document.querySelector('.nav-links')
-    const navLinks = document.querySelectorAll('.nav-links li');
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //Toggle Nav
+  burger.addEventListener("click", () => {
     //Toggle Nav
-    burger.addEventListener('click', ()=>{
-        
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
+    nav.classList.toggle("nav-active");
 
-        //Animation Links
-        navLinks.forEach((link, index) => {
-            if(link.style.animation)
-            {
-                link.style.animation = '';
-            }
-            else
-            {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
-            }
+    //Animation Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
+          0.5}s`;
+      }
 
-            console.log(index/7);
-        });
-        
-        //Burger Animation
-        burger.classList.toggle('toggle');
-
+      console.log(index / 7);
     });
 
-}
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
 navSlide();
